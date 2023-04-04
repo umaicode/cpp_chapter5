a46eb620f4223aa44de1f2d48187b8564b57401f
umaicode <park.seungchan99@gmail.com>
2023-04-04 12:12:00 +0900

    230404

--- student_object.cpp
+++ student_object.cpp
@@ -9,15 +9,25 @@ class Student {
 public:
 	Student(char* sid, char* sname, int age) : age(age)
 	{
-		this->sid = sid;
-		this->sname = sname;
+		// 주소를 가리킨다.
+		// this->sid = sid;
+		// this->sname = sname;
+
+		// 자신이 있는 공간을 가리킨다. 
+		this->sid = new char[sizeof(sid)];
+		strcpy(this->sid, sid);
+		this->sname = new char[sizeof(sname)];
+		strcpy(this->sname, sname);
 	}
 	friend ostream& operator<<(ostream&, Student& s);
-	~Student() {};
+	~Student() {
+		delete[] sid;
+		delete[] sname;
+	};
 };
 ostream& operator<<(ostream& os, Student& s) {
-	os << "student:: "<< s.sid<<" "<<s.sname<<" "<<s.age<<endl;
-		return os;
+	os << "student:: " << s.sid << " " << s.sname << " " << s.age << endl;
+	return os;
 }
 int main() {
 	cout << "이것은 테스트 프로그램" << endl;
